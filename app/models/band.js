2aeab80fb319749af47e7b33880d96a22e8084ac
Balint Erdi <balint.erdi@gmail.com>
2016-09-07 17:30:53 +0200

    Make `band hasMany songs` a sync relationship

--- app/models/band.js
+++ app/models/band.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string'),
-  description: DS.attr('string'),
-  songs: DS.hasMany('song'),
+  name:         DS.attr('string'),
+  description:  DS.attr('string'),
+  songs:        DS.hasMany('song', { async: false }),
 });
