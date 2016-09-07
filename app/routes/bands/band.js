ef4d92e0bae72447a85f9f9844860293815b7a15
Balint Erdi <balint.erdi@gmail.com>
2016-09-07 15:43:11 +0200

    Stop the flicker on songs by pre-fetching songs.
    The band needs to be reloaded in the band route so that an ajax request is fired
    to the backend and thus the include: 'songs' parameter can be applied (and thus
    the songs fetched).

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('band', params.id);
+    return this.store.findRecord('band', params.id, {
+      reload: true,
+      include: 'songs'
+    });
   },
 });
