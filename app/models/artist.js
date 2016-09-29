86d2c6110d5fec6db587a49cd2fb8bf560bea988
Balint Erdi <balint.erdi@gmail.com>
2016-09-29 17:08:17 +0200

    Make a decent artist page

--- app/models/artist.js
+++ app/models/artist.js
@@ -1,6 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstName:  DS.attr('string'),
-  lastName:   DS.attr('string'),
+  firstName:   DS.attr('string'),
+  lastName:    DS.attr('string'),
+  yearOfBirth: DS.attr('number'),
+  imageUrl:    DS.attr('string'),
+
+  name: Ember.computed('firstName', 'lastName', function() {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }),
+
+  age: Ember.computed('yearOfBirth', function() {
+    return new Date().getFullYear() - this.get('yearOfBirth');
+  })
 });
--- config/environment.js
+++ config/environment.js
@@ -23,8 +23,8 @@ module.exports = function(environment) {
     ENV.DS_WARN_ON_UNKNOWN_KEYS = true;
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
