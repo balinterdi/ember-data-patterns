824137c7f6c2f78b7404dd9f099cc4f39c127493
Balint Erdi <balint.erdi@gmail.com>
2016-09-29 16:02:07 +0200

    Show a spinner while loading new artists

--- app/routes/artists.js
+++ app/routes/artists.js
@@ -1,8 +1,26 @@
+import Ember from 'ember';
 import Route from 'ember-route';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('artist', { reload: true });
+    let artists = this.store.peekAll('artist');
+    if (artists.get('length') === 0) {
+      return this.store.findAll('artist');
+    } else {
+      Ember.run.scheduleOnce('afterRender', this, this._fetchMoreArtists);
+      return artists;
+    }
+  },
+
+  _fetchMoreArtists() {
+    let { store, controller } = this;
+    controller.set('isLoadingArtists', true);
+    store.findAll('artist', { reload: true }).then((artists) => {
+      controller.set('model', artists);
+    })
+    .finally(() => {
+      controller.set('isLoadingArtists', false);
+    });
   }
 });
 
